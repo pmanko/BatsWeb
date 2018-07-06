d9430ce9bc7b66f4b17b83df4b9da90e2265d4d1
Piotr Mankowski <piotr.mankowski@gmail.com>
2018-07-06 02:04:32 -0400

    Some cleanup

---
+++ puckswebIOS/puckswebIOS/Main.cs
@@ -0,0 +1,18 @@
+﻿using UIKit;
+
+namespace puckswebIOS
+{
+    public class Application
+    {
+        UIWebView webView;
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+            webView = new UIWebView(View.Bounds);
+            View.AddSubview(webView);
+        }
+    }
+}
